a3cac5939ac6f367c9f6c2e2988b2f3bd569f651
Bisousnours <agnesgaspard@hotmail.fr>
2019-01-29 07:08:44 +0100

    Try to fix the builds error by removing the excess

--- src/test/java/esiea/AppTest.java
+++ src/test/java/esiea/AppTest.java
@@ -2,38 +2,11 @@ package esiea;
 
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions.*;
-//import junit.framework.TestCase;
-//import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
 
 public class AppTest 
 
 {
 	@Test
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
 	Assertions.assertThat(true).isTrue();
