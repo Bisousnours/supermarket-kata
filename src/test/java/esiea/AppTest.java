aec3d064764a17dbf0f0eac168b95f7fd03f9efa
MatteoDelmaestro <43181263+MatteoDelmaestro@users.noreply.github.com>
2019-01-29 19:22:11 +0100

    try to fix import in AppTest.java

--- src/test/java/esiea/AppTest.java
+++ src/test/java/esiea/AppTest.java
@@ -2,7 +2,16 @@ package esiea;
 
 import org.junit.jupiter.api.Test;
 //import org.assertj.core.api.Assertions.*;
-import org.assertj.core.api.Assertions.assertThat;
+import  org.assertj.core.api.Assertions.assertThat;  // main one
+import  org.assertj.core.api.Assertions.atIndex; // for List assertions
+import  org.assertj.core.api.Assertions.entry;  // for Map assertions
+import  org.assertj.core.api.Assertions.tuple; // when extracting several properties at once
+import  org.assertj.core.api.Assertions.fail; // use when writing exception tests
+import  org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown; // idem
+import  org.assertj.core.api.Assertions.filter; // for Iterable/Array assertions
+import  org.assertj.core.api.Assertions.offset; // for floating number assertions
+import  org.assertj.core.api.Assertions.anyOf; // use with Condition
+import  org.assertj.core.api.Assertions.contentOf; // use with File assertions
 
 public class AppTest 
 
@@ -12,4 +21,6 @@ public class AppTest
     {
 	Assertions.assertThat(true).isTrue();
     }
+
 }
+
