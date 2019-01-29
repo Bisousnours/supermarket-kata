9392446f5d912034ea68c2cde71961c287c12e33
MatteoDelmaestro <43181263+MatteoDelmaestro@users.noreply.github.com>
2019-01-29 19:41:33 +0100

    try to fix bug in import in AppTest (delete some import)

--- src/test/java/esiea/AppTest.java
+++ src/test/java/esiea/AppTest.java
@@ -1,17 +1,8 @@
 package esiea;
 
 import org.junit.jupiter.api.Test;
-//import org.assertj.core.api.Assertions.*;
+import org.assertj.core.api.Assertions;
 import  org.assertj.core.api.Assertions.assertThat;  // main one
-import  org.assertj.core.api.Assertions.atIndex; // for List assertions
-import  org.assertj.core.api.Assertions.entry;  // for Map assertions
-import  org.assertj.core.api.Assertions.tuple; // when extracting several properties at once
-import  org.assertj.core.api.Assertions.fail; // use when writing exception tests
-import  org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown; // idem
-import  org.assertj.core.api.Assertions.filter; // for Iterable/Array assertions
-import  org.assertj.core.api.Assertions.offset; // for floating number assertions
-import  org.assertj.core.api.Assertions.anyOf; // use with Condition
-import  org.assertj.core.api.Assertions.contentOf; // use with File assertions
 
 public class AppTest 
 
