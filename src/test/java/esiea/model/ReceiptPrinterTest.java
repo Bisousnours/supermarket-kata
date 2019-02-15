2c619622ed03a4c0e36c11df368086be18cb9302
MatteoDelmaestro <43181263+MatteoDelmaestro@users.noreply.github.com>
2019-02-15 14:14:09 +0100

    update test receiptPrinter for more codecov (discount part)

--- src/test/java/esiea/model/ReceiptPrinterTest.java
+++ src/test/java/esiea/model/ReceiptPrinterTest.java
@@ -22,13 +22,15 @@ public class ReceiptPrinterTest {
         cart.addItemQuantity(toothbrush, 4);
         cart.addItemQuantity(toothpaste, 1);
 
+
         Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 0.99);
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         ReceiptPrinter receiptPrinter = new ReceiptPrinter();
 
         String stringToTest = receiptPrinter.printReceipt(receipt);
-        String expectedString = "toothbrush"+"                          "+" * "+"\n"+" "+"0.99"+" * "+"4"+"\n"+"toothpaste"+"                          "+" * "+"\n";
+        String expectedString = "toothbrush"+"                          "+" * "+"\n"+" "+"0.99"+" * "+"4"+"\n"+"toothpaste"+"                          "+" * "+"\n"+"("+"toothbrush"+")"+"                "+"-"+"0.99"+"\n";
 
 
         Assertions.assertEquals(expectedString, stringToTest);
