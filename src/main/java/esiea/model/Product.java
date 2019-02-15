38898094c5d32679aa25755b5ef03217e06a1135
bisousnours <agnesgaspard@hotmail.fr>
2019-02-15 10:43:28 +0100

    Product tests ok

--- src/main/java/esiea/model/Product.java
+++ src/main/java/esiea/model/Product.java
@@ -19,19 +19,4 @@ public class Product {
     public ProductUnit getUnit() {
         return unit;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(name, product.name) &&
-                unit == product.unit;
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name, unit);
-    }
 }
